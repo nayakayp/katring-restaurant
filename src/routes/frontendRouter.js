f9164cf08fb24c93e5fd45b420afcc209fa9cebd
nayakayp <nayakayp@gmail.com>
2020-09-12 20:19:41 +0700

    finished frontend design

--- src/routes/frontendRouter.js
+++ src/routes/frontendRouter.js
@@ -11,25 +11,26 @@ router.get("/", function (req, res) {
 
 // GET Menu
 router.get("/menu", function (req, res) {
-  res.render("menu.ejs", {
-    footerBgColor: "#201416",
-    footerLinkColor: "#f3ebde",
-  });
+  res.render("menu.ejs", {});
 });
 
 // GET Profil
-router.get("/", function (req, res) {
+router.get("/profil", function (req, res) {
   res.render("profil.ejs", {});
 });
 
 // GET Hubungi Kami
-router.get("/kontak", function (req, res) {
+router.get("/hubungi-kami", function (req, res) {
   res.render("hubungi-kami.ejs", {});
 });
 
 // GET Cart
-router.get("keranjang", function (req, res) {
+router.get("/keranjang", function (req, res) {
   res.render("keranjang.ejs", {});
 });
 
+// GET Detail Produk
+router.get("/menu/produk", function (req, res) {
+  res.render("detail-produk.ejs", {});
+});
 module.exports = router;
